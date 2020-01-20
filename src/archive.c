287e7e02aac5fa95e14c616a46a7c3a729eacbff
Jacob Gelling <jacobgelling@users.noreply.github.com>
2020-01-20 23:54:28 +0100

    Remove Windows specific string functions

--- src/archive.c
+++ src/archive.c
@@ -128,15 +128,9 @@ int unpack(const char *archive_path, const char *folder_path) {
         // Create file path
         const size_t file_path_size = strlen(filename) + strlen(folder_path) + 2;
         char *file_path = malloc(file_path_size);
-        #ifdef _WIN32
-            strcpy_s(file_path, file_path_size, folder_path);
-            strcat_s(file_path, file_path_size, "/");
-            strcat_s(file_path, file_path_size, filename);
-        #else
-            strcpy(file_path, folder_path);
-            strcat(file_path, "/");
-            strcat(file_path, filename);
-        #endif
+        strcpy(file_path, folder_path);
+        strcat(file_path, "/");
+        strcat(file_path, filename);
 
         // Extract the file
         if (compression_level == 0) {
